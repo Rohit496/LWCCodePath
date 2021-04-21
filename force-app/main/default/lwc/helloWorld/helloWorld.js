cf3c3986e8f6206cba1d34d867619ccf98a77a4d
Rohit Kumar <rohitdotnet@outlook.com>
2021-04-21 16:40:21 +0530

    node js installed

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,13 @@
+/**
+ * @description       :
+ * @author            : Rohit Kumar
+ * @group             :
+ * @last modified on  : 21-04-2021
+ * @last modified by  : Rohit Kumar
+ * Modifications Log
+ * Ver   Date         Author        Modification
+ * 1.0   21-04-2021   Rohit Kumar   Initial Version
+ **/
+import { LightningElement } from "lwc";
+
+export default class HelloWorld extends LightningElement {}
